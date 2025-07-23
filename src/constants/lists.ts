e8c6e414da0a0c3cd6e272120d2a64499004d945
Warriorcarl <warriorcarl@yahoo.com>
2025-07-23 15:25:56 +0700

    update tokenlist

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
 // Helios Default Token List
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://gist.githubusercontent.com/Warriorcarl/f738e1fbcb367ed674cf8c303d40280b/raw/f918d542c7eb743fe6e27357400c37503ae972fd/HLSDEX-V2.json'
+  'https://gist.githubusercontent.com/Warriorcarl/f738e1fbcb367ed674cf8c303d40280b/raw/fc71c13724fbf7f0b8929ebc934f621148aaf8c1/HLSDEXV2.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
